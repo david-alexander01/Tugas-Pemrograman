d52357817986330840629a55e375ddf09696303e
David Alexander <david.alexander01@ui.ac.id>
2021-03-26 12:09:14 +0700

    Implemented class Mahasiswa and Matakuliah

--- assignment2/src/main/java/assignments/assignment2/Mahasiswa.java
+++ assignment2/src/main/java/assignments/assignment2/Mahasiswa.java
@@ -1,5 +1,7 @@
 package assignments.assignment2;
 
+import java.util.Arrays;
+
 public class Mahasiswa {
     private MataKuliah[] mataKuliah = new MataKuliah[10];
     private String[] masalahIRS;
@@ -8,26 +10,56 @@ public class Mahasiswa {
     private String jurusan;
     private long npm;
 
-    public Mahasiswa(String nama, long npm){
-        /* TODO: implementasikan kode Anda di sini */
-    }
-    
-    public void addMatkul(MataKuliah mataKuliah){
-        /* TODO: implementasikan kode Anda di sini */
-    }
-
-    public void dropMatkul(MataKuliah mataKuliah){
-        /* TODO: implementasikan kode Anda di sini */
+    public Mahasiswa(String nama, long npm) {
+        // Initialize attributes
+        this.nama = nama;
+        this.npm = npm;
+        // TODO: find a way to import this.jurusan = getJurusan(npm);
     }
 
-
-    public void cekIRS(){
+    public void addMatkul(MataKuliah mataKuliah) {
         /* TODO: implementasikan kode Anda di sini */
+        if (this.mataKuliah[9] != null)
+            System.out.println("[DITOLAK] Maksimal mata kuliah yang diambil hanya 10.");
+        else if (Arrays.binarySearch(this.mataKuliah, mataKuliah) >= 0)
+            System.out.printf("[DITOLAK] %s telah diambil sebelumnya.", mataKuliah);
+        else if (!mataKuliah.available())
+            System.out.printf("[DITOLAK] %s telah penuh kapasitasnya.", mataKuliah);
+        else {
+            mataKuliah.addMahasiswa(this);
+            totalSKS += mataKuliah.getSKS();
+        }
     }
+
+    public void dropMatkul(MataKuliah mataKuliah) {
+        /* TODO: implementasikan kode Anda di sini */
+        if (Arrays.binarySearch(this.mataKuliah, mataKuliah) < 0)
+            System.out.printf("[DITOLAK] %s belum pernah diambil.", mataKuliah);
+        else {
+            mataKuliah.dropMahasiswa(this);
+            totalSKS -= mataKuliah.getSKS();
+        }
+    }
+
+
+    public void cekIRS() {
+        /* TODO: implementasikan kode Anda di sini */
+        masalahIRS = new String[10];
+        int index = 0;
+        for (MataKuliah mataKuliah : this.mataKuliah) {
+            if (!mataKuliah.getKode().equals("CS") && !mataKuliah.getKode().equals(this.jurusan)){
+                masalahIRS[index] = "Mata kuliah " + mataKuliah + " tidak dapat diambil jurusan " + this.jurusan;
+                index += 1;
+            }
+        }
+        if (this.totalSKS > 24)
+            masalahIRS[index] = "SKS yang Anda ambil lebih dari 24";
+    }
+
 
     public String toString() {
         /* TODO: implementasikan kode Anda di sini */
-        return "";
+        return this.nama;
     }
 
 }
--- assignment2/src/main/java/assignments/assignment2/MataKuliah.java
+++ assignment2/src/main/java/assignments/assignment2/MataKuliah.java
@@ -1,5 +1,7 @@
 package assignments.assignment2;
 
+import java.util.Arrays;
+
 public class MataKuliah {
     private String kode;
     private String nama;
@@ -7,23 +9,54 @@ public class MataKuliah {
     private int kapasitas;
     private Mahasiswa[] daftarMahasiswa;
 
-    public MataKuliah(String kode, String nama, int sks, int kapasitas){
-        /* TODO: implementasikan kode Anda di sini */
+    public MataKuliah(String kode, String nama, int sks, int kapasitas) {
+        // Initialize attributes
+        this.kode = kode;
+        this.nama = nama;
+        this.sks = sks;
+        this.kapasitas = sks;
+        this.daftarMahasiswa = new Mahasiswa[kapasitas];
     }
 
+    // temporary test method
+    // TODO: REMOVE THIS
+    public static void main(String[] args) {
+        MataKuliah a = new MataKuliah("IK", "POK", 3, 100);
+        System.out.println("TEST SOUT " + a);
+    }
 
     public void addMahasiswa(Mahasiswa mahasiswa) {
-        /* TODO: implementasikan kode Anda di sini */
-
+        // Add new mahasiswa to list, keeps list sorted
+        int key = -Arrays.binarySearch(daftarMahasiswa, mahasiswa) - 1;
+        for (int i = daftarMahasiswa.length; i > key; i--) {
+            daftarMahasiswa[i] = daftarMahasiswa[i - 1];
+        }
+        daftarMahasiswa[key] = mahasiswa;
     }
 
     public void dropMahasiswa(Mahasiswa mahasiswa) {
-        /* TODO: implementasikan kode Anda di sini */
+        // Remove mahasiswa from list, keeps list sorted
+        int key = Arrays.binarySearch(daftarMahasiswa, mahasiswa);
+        daftarMahasiswa[key] = null;
+        for (int i = key; i < daftarMahasiswa.length; i++) {
+            daftarMahasiswa[i] = daftarMahasiswa[i + 1];
+        }
+    }
 
+    public boolean available() {
+        return daftarMahasiswa[kapasitas - 1] == null;
     }
 
     public String toString() {
         /* TODO: implementasikan kode Anda di sini */
-        return "";
+        return this.nama;
+    }
+
+    public int getSKS() {
+        return this.sks;
+    }
+
+    public String getKode() {
+        return this.kode;
     }
 }
