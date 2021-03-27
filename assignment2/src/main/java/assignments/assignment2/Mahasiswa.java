b196108040870e5223288f7ddb90466e49b7cbe2
David Alexander <david.alexander01@ui.ac.id>
2021-03-27 03:20:55 +0000

    add documentations and fix false nulls

--- assignment2/src/main/java/assignments/assignment2/Mahasiswa.java
+++ assignment2/src/main/java/assignments/assignment2/Mahasiswa.java
@@ -14,25 +14,8 @@ public class Mahasiswa {
         this.jurusan = this.getJurusan();
     }
 
-    public String getJurusan() {
-        // Returns string of jurusan else "invalid" if code is invalid.
-        String jurusanCode = (npm + "").substring(2, 4);
-        return switch (jurusanCode) {
-            case "01" -> "Ilmu Komputer";
-            case "02" -> "Sistem Informasi";
-            default -> "invalid";
-        };
-    }
-
-    public long getNPM() {
-        return npm;
-    }
-
-    public MataKuliah[] getMataKuliah() {
-        return mataKuliah;
-    }
-
     private int findEmptyMataKuliah() {
+        // Find empty index in mataKuliah array, returns -1 if full
         for (int i = 0; i < mataKuliah.length; i++) {
             if (mataKuliah[i] == null)
                 return i;
@@ -41,6 +24,7 @@ public class Mahasiswa {
     }
 
     private int findMataKuliahIndex(MataKuliah mataKuliah) {
+        // Find index for a given mataKuliah
         for (int i = 0; i < this.mataKuliah.length; i++) {
             if (this.mataKuliah[i] == mataKuliah)
                 return i;
@@ -48,8 +32,19 @@ public class Mahasiswa {
         return -1;
     }
 
+    private boolean isTakingMataKuliah(MataKuliah mataKuliah) {
+        // Returns true if mataKuliah in mataKuliah array else false
+        for (MataKuliah element : this.mataKuliah) {
+            if (element == mataKuliah) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     public void addMatkul(MataKuliah mataKuliah) {
-        if (this.mataKuliah[9] != null)
+        // Adds mataKuliah to first available index of mataKuliah array if possible
+        if (findEmptyMataKuliah < 0)
             System.out.println("[DITOLAK] Maksimal mata kuliah yang diambil hanya 10.");
         else if (this.isTakingMataKuliah(mataKuliah))
             System.out.printf("[DITOLAK] %s telah diambil sebelumnya.", mataKuliah);
@@ -63,6 +58,7 @@ public class Mahasiswa {
     }
 
     public void dropMatkul(MataKuliah mataKuliah) {
+        // Drops mataKuliah if possible
         if (!this.isTakingMataKuliah(mataKuliah))
             System.out.printf("[DITOLAK] %s belum pernah diambil.", mataKuliah);
         else {
@@ -72,13 +68,13 @@ public class Mahasiswa {
         }
     }
 
-
     public void cekIRS() {
+        // Find and print problems in IRS. Possible problems: mataKuliah code not according to jurusan, SKS > 24.
         String[] masalahIRS = new String[20];
         int index = 0;
         for (MataKuliah mataKuliah : this.mataKuliah) {
             if (mataKuliah == null)
-                break;
+                continue;
             if (!mataKuliah.getKode().equals("CS") || !mataKuliah.getKode().equals(this.jurusan)) {
                 masalahIRS[index] = "Mata kuliah " + mataKuliah + " tidak dapat diambil jurusan " + this.jurusan;
                 index += 1;
@@ -97,21 +93,34 @@ public class Mahasiswa {
             System.out.println("IRS tidak bermasalah.");
     }
 
-
     public String toString() {
+        // Returns name
         return this.nama;
     }
 
     public int getSKS() {
+        // Returns totalSKS
         return this.totalSKS;
     }
 
-    private boolean isTakingMataKuliah(MataKuliah mataKuliah) {
-        for (MataKuliah element : this.mataKuliah) {
-            if (element == mataKuliah) {
-                return true;
-            }
-        }
-        return false;
+    public long getNPM() {
+        // Returns npm
+        return npm;
     }
+
+    public MataKuliah[] getMataKuliah() {
+        // Returns array of mataKuliah
+        return mataKuliah;
+    }
+
+    public String getJurusan() {
+        // Returns string of jurusan else "invalid" if code is invalid.
+        String jurusanCode = (npm + "").substring(2, 4);
+        return switch (jurusanCode) {
+            case "01" -> "Ilmu Komputer";
+            case "02" -> "Sistem Informasi";
+            default -> "invalid";
+        };
+    }
+
 }
